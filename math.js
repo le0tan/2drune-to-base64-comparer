3633a49e224699768cf5449e455981be57786445
Tan Yuanhong <leotan.yh@gmail.com>
2018-09-03 23:51:01 +0800

    added math_xx naming convention support for curve missions

---
+++ math.js
@@ -0,0 +1,9 @@
+function math_sin(a){
+    return Math.sin(a);
+}
+
+function math_cos(a){
+    return Math.cos(a);
+}
+
+const math_PI = Math.PI;
--- test_code.js
+++ test_code.js
@@ -1,31 +1,33 @@
 function test_function(){
-    function steps(r1, r2, r3, r4){
-        // your answer here
-        return overlay(overlay(translate(-0.5, -0.5, scale(0.5, r4)),
-                               translate(-0.5,  0.5, scale(0.5, r3))), 
-                       overlay(translate( 0.5,  0.5, scale(0.5, r2)),
-                               translate( 0.5, -0.5, scale(0.5, r1))));
+    function s_generator(pt) {
+        const x = x_of(pt);
+        const y = y_of(pt);
+        function helper(t){
+            if(t <= 1/2){
+                const term1 = (t * 2 * (3 / 4));
+                return make_point(math_cos(2 * math_PI * term1),
+                                  math_sin(2 * math_PI * term1) + 1);
+            } else {
+                const term2 = -(t * 2 * (3 / 4));
+                return make_point(math_cos(2 * math_PI * term2),
+                                  math_sin(2 * math_PI * term2) - 1);
+            }
+        }
+        return helper;
     }
     
-    // Test
-    return show(steps(rcross_bb, sail_bb, corner_bb, nova_bb));
+    return (draw_connected_squeezed_to_window(200))(s_generator(make_point(0.5, 0.25)));
 }
 
 function target_function(){
-    function place_rune(trans1, trans2, factor, rune){
-        return translate(trans1, trans2,
-                        scale(factor, rune));
-    }
-    
-    function steps(r1, r2, r3, r4){
-        // your answer here
-       return overlay(
-                  overlay(place_rune(-0.5, -0.5, 0.5, r4),
-                           place_rune(-0.5, 0.5, 0.5, r3)),
-                  overlay(place_rune(0.5, 0.5, 0.5, r2),
-                          place_rune(0.5, -0.5, 0.5, r1))); 
+    function s_generator(pt) {
+        return t => t <= 0.5
+                    ? make_point(math_sin(0.5 * math_PI - 3 * math_PI * t) + x_of(pt),
+                                 math_cos(0.5 * math_PI - 3 * math_PI * t) + y_of(pt) + 1)
+                    : make_point(math_sin(3 * math_PI * (t - 0.5)) + x_of(pt),
+                                 math_cos(3 * math_PI * (t - 0.5)) + y_of(pt) - 1);
     }
     
     // Test
-    return show(steps(rcross_bb, sail_bb, corner_bb, nova_bb));
+    return (draw_connected_squeezed_to_window(200))(s_generator(make_point(0.5, 0.25)));       
 }
